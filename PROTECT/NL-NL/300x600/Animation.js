bf8067fb581f532428bcd70daf8da2af162d9898
Henry Richardson <hrichardson83@gmail.com>
2015-10-20 18:40:55 -0400

    FR-CA and FR-FR complete

--- PROTECT/NL-NL/300x600/Animation.js
+++ PROTECT/NL-NL/300x600/Animation.js
@@ -51,16 +51,16 @@ home_animation = (function() {
 	
 	function frame1()
 	{
-		TweenLite.to(first_text, 1, {top:-300, delay: 2});
-		TweenLite.to(product_text, 1.1, {top:-165, delay:2});		
-		TweenLite.to(bg, 1.1, {top:-145, scaleX:1, scaleY:1, delay:2});		
+		TweenLite.to(first_text, .8, {top:-300, delay: 2});
+		TweenLite.to(product_text, 1, {top:-165, delay:2});		
+		TweenLite.to(bg, 1, {top:-145, scaleX:1, scaleY:1, delay:2});		
 		TweenLite.delayedCall(2, frame2);
 	}
 	
 	function frame2()
 	{	
-		TweenLite.to(white_BG, 1.15, {top:425, delay:0});
-		TweenLite.to(logo, 1, {opacity:1, delay:1});
+		TweenLite.to(white_BG, 1, {top:425, delay:0});
+		TweenLite.to(logo, .9, {opacity:1, delay:1});
 		TweenLite.to(second_text, 3, {opacity:1, delay:1.2});
 		TweenLite.to(third_text, 3, {opacity:1, delay:2.5});
 		TweenLite.delayedCall(4, frame3);
